14a9b5f640746e827be89693cac24877e95722b3
Andreas Boräng <andbor02@gmail.com>
2022-09-27 10:11:44 +0200

    Added Tile backend

--- IoT-WPF/ControlSystem/Components/DeviceTile.xaml.cs
+++ IoT-WPF/ControlSystem/Components/DeviceTile.xaml.cs
@@ -24,5 +24,47 @@ namespace ControlSystem.Components
         {
             InitializeComponent();
         }
+
+        public static readonly DependencyProperty DeviceTitleProperty = DependencyProperty.Register("DeviceTitle", typeof(string), typeof(DeviceTile));
+        public string DeviceTitle
+        {
+            get { return (string)GetValue(DeviceTitleProperty); }
+            set { SetValue(DeviceTitleProperty, value); }
+        }
+
+        public static readonly DependencyProperty DeviceTypeProperty = DependencyProperty.Register("DeviceType", typeof(string), typeof(DeviceTile));
+        public string DeviceType
+        {
+            get { return (string)GetValue(DeviceTypeProperty); }
+            set { SetValue(DeviceTypeProperty, value); }
+        }
+
+        public static readonly DependencyProperty DeviceIconActiveProperty = DependencyProperty.Register("DeviceIconActive", typeof(string), typeof(DeviceTile));
+        public string DeviceIconActive
+        {
+            get { return (string)GetValue(DeviceIconActiveProperty); }
+            set { SetValue(DeviceIconActiveProperty, value); }
+        }
+
+        public static readonly DependencyProperty DeviceIconInactiveProperty = DependencyProperty.Register("DeviceIconInactive", typeof(string), typeof(DeviceTile));
+        public string DeviceIconInactive
+        {
+            get { return (string)GetValue(DeviceIconInactiveProperty); }
+            set { SetValue(DeviceIconInactiveProperty, value); }
+        }
+
+        public static readonly DependencyProperty DeviceFontInactiveProperty = DependencyProperty.Register("DeviceFontActive", typeof(string), typeof(DeviceTile));
+        public string DeviceFontActive
+        {
+            get { return (string)GetValue(DeviceFontInactiveProperty); }
+            set { SetValue(DeviceFontInactiveProperty, value); }
+        }
+
+        public static readonly DependencyProperty DeviceFontActiveProperty = DependencyProperty.Register("DeviceFontInactive", typeof(string), typeof(DeviceTile));
+        public string DeviceFontInactive
+        {
+            get { return (string)GetValue(DeviceFontActiveProperty); }
+            set { SetValue(DeviceFontActiveProperty, value); }
+        }
     }
 }
