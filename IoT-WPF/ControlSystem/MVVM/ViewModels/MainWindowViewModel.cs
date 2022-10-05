837e284c90f2e8ccee0c8fccab050f81c5b0bea6
Andreas Boräng <andbor02@gmail.com>
2022-10-05 10:15:50 +0200

    Fixed window not changing view bug

--- IoT-WPF/ControlSystem/MVVM/ViewModels/MainWindowViewModel.cs
+++ IoT-WPF/ControlSystem/MVVM/ViewModels/MainWindowViewModel.cs
@@ -2,29 +2,27 @@
 
 public class MainWindowViewModel : ObservableObject
 {
+    private object _currentView;
+    public RelayCommand KitchenViewCommand { get; set; }
+    public RelayCommand BedroomViewCommand { get; set; }
+    public RelayCommand LivingRoomViewCommand { get; set; }
+    public KitchenViewModel KitchenViewModel { get; set; }
+    public BedroomViewModel BedroomViewModel { get; set; }
+    public LivingRoomViewModel LivingRoomViewModel { get; set; }
+
 
     public MainWindowViewModel()
     {
+        KitchenViewModel = new KitchenViewModel();
         BedroomViewModel = new BedroomViewModel();
-        KitchenViewModel = new BedroomViewModel();
-        LivingRoomViewModel = new BedroomViewModel();
+        LivingRoomViewModel = new LivingRoomViewModel();
+
+        KitchenViewCommand = new RelayCommand(x => { CurrentView = KitchenViewModel; });
+        BedroomViewCommand = new RelayCommand(x => { CurrentView = BedroomViewModel; });
+        LivingRoomViewCommand = new RelayCommand(x => { CurrentView = LivingRoomViewModel; });
 
         CurrentView = BedroomViewModel;
-
-        BedroomViewCommand = new RelayCommand(x => { CurrentView = BedroomViewModel; });
-        KitchenViewCommand = new RelayCommand(x => { CurrentView = KitchenViewModel; });
-        LivingRoomViewCommand = new RelayCommand(x => { CurrentView = LivingRoomViewModel; });
     }
-
-    private object _currentView;
-
-    public RelayCommand BedroomViewCommand { get; set; }
-    public BedroomViewModel BedroomViewModel { get; set; }
-    public RelayCommand KitchenViewCommand { get; set; }
-    public BedroomViewModel KitchenViewModel { get; set; }
-    public RelayCommand LivingRoomViewCommand { get; set; }
-    public BedroomViewModel LivingRoomViewModel { get; set; }
-
 
     public object CurrentView
     {
